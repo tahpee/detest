9cf3a434ee17d4867e8cc49bc501ae0f29535f59
Tim Ahpee <tim@ahpee.org>
2015-09-01 20:38:05 +1000

    Cleaned up the urls (now that I know how to use the url template tag)

--- detest/detest_ui/urls.py
+++ detest/detest_ui/urls.py
@@ -7,12 +7,11 @@ urlpatterns = patterns('',
                        url(r'^index.html$', views.index, name='index'),
                        url(r'^dashboard/(?P<project>[\w\s]+)/$', views.project, name='project'),
                        url(r'^design/(?P<project>[\w\s]+)/$', views.design_top, name='project'),
-                       url(r'^projects$', views.project_list, name='project_list'),
                        url(r'^buttons/$', views.buttons, name='buttons'),
 
                        url(r'^admin/projects$', admin_views.project_list, name='project_list'),
                        url(r'^admin/new_project$', admin_views.new_project, name='new_project'),
                        url(r'^admin/delete_project$', admin_views.delete_project, name='delete_project'),
-                       url(r'^admin/project_details/(?P<project>\d+)/$', admin_views.project_view, name='project'),
+                       url(r'^admin/project_details/(?P<project>\d+)/$', admin_views.project_view, name='project_details'),
 
                        )
